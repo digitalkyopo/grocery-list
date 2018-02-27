b49ffa3c45f0914dc4901b4f18ef4df0057519db
Jeremy Kim <thk116@gmail.com>
2018-02-27 03:46:49 +0000

    removed new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { NewcompGrocerylistComponent } from './newcomp-grocerylist/newcomp-grocerylist.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewcompGrocerylistComponent
   ],
   imports: [
     BrowserModule
